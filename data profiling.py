44668dacae2316c4588ab41a3d2a28f1ac3a322b
adjii <desykaadjisafitri508@gmail.com>
2021-10-08 15:09:31 +0700

    Data Quality

--- data profiling.py
+++ data profiling.py
@@ -53,4 +53,11 @@ print('Mean value: ', retail_raw['item_price'].mean())
 print('Median value: ', retail_raw['item_price'].median())
 print('Standard Deviation value: ', retail_raw['item_price'].std())
 
-# Quantile 
+# Quantile statistics kolom quantity
+print('Kolom quantity: ')
+print(retail_raw['quantity'].quantile([0.25, 0.5, 0.75]))
+
+# Tugas praktek: Quantile statistics kolom item_price
+print('')
+print('Kolom item_price: ')
+print(retail_raw['item_price'].quantile([0.25, 0.5, 0.75]))
