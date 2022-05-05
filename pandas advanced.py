04d56d351844265416837b435bec1df0c93d0a38
adjii <desykaadjisafitri508@gmail.com>
2022-05-06 00:31:38 +0700

    Data Manipulation

--- pandas advanced.py
+++ pandas advanced.py
@@ -31,5 +31,68 @@ print("Column-wise - concat:\n", col_wise_concat)
 multiindex_concat = pd.concat([df2,df1], axis=0, keys=['df2','df1'])
 print("Multiindex - concat:\n", multiindex_concat)
 # %% [markdown]
-# 
+# __Merge - Part 1__
+# %%
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+    'key':['k1','k2','k3','k4','k5'],
+    'val1':[200, 500, 0, 500, 100],
+    'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+    'key':['k1','k3','k5','k7','k10'],
+    'val3':[1,2,3,4,5],
+    'val4':[6,7,8,8,10]
+})
+# Merge yang ekivalen dengan SQL left join
+merge_df_left = pd.merge(left=df2, right=df1, how='left', left_on='key', right_on='key')
+print('Merge - Left:\n', merge_df_left)
+# Merge yang ekivalen dengan SQL right join
+merge_df_right = pd.merge(left=df2, right=df1, how='right', left_on='key', right_on='key')
+print('Merge - Right:\n', merge_df_right)
+# Merge yang ekivalen dengan SQL inner join
+merge_df_inner = pd.merge(left=df2, right=df1, how='inner', left_on='key', right_on='key')
+print('Merge - Inner:\n', merge_df_inner)
+# Merge yang ekivalen dengan SQL outer join
+merge_df_outer = pd.merge(left=df2, right=df1, how='outer', left_on='key', right_on='key')
+print('Merge - Outer:\n', merge_df_outer)
+# %% [markdown]
+# __Merge - Part 2__
+# %%
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+    'key':['k1','k2','k3','k4','k5'],
+    'val1':[200, 500, 0, 500, 100],
+    'val2':[30, 50, 100, 20, 10]
+}).set_index(['key','val2'])
+print('Dataframe 1:\n', df1)
+df2 = pd.DataFrame({
+    'key':['k1','k3','k5','k7','k10'],
+    'val3':[1,2,3,4,5],
+    'val4':[6,7,8,8,10]
+}).set_index(['key','val3'])
+print('Dataframe 2:\n', df2)
+# Merge dataframe yang memiliki multi index
+df_merge = pd.merge(df1.reset_index(),df2.reset_index())
+print('Merging dataframe:\n', df_merge)
+# %% [markdown]
+# __Join__
+# %%
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+    'key':['k1','k2','k3','k4','k5'],
+    'val1':[200, 500, 0, 500, 100],
+    'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+    'key':['k1','k3','k5','k7','k10'],
+    'val3':[1,2,3,4,5],
+    'val4':[6,7,8,8,10]
+})
+# Penerapan join dengan menggunakan set_index dan keyword how
+join_df = df1.set_index('key').join(df2.set_index('key'), how='outer')
+print(join_df)
 # %%
