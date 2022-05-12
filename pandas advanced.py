3147ef19d2ab2feb97f8f7f4a0b0c98eccb68baf
adjii <desykaadjisafitri508@gmail.com>
2022-05-12 13:00:51 +0700

    Data Manipulation

--- pandas advanced.py
+++ pandas advanced.py
@@ -307,4 +307,17 @@ print(gaq.head())
 # Cetak info dari dataframe gaq
 print('info')
 print(gaq.info()) 
+# %% [markdown]
+# __Convert to Datetime__
+# %%
+import pandas as pd
+# Load dataset https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+# Cetak 5 data teratas
+print('Sebelum diubah dalam format datetime:\n', gaq.head())
+# Ubah menjadi datetime
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+# Cetak 5 data teratas
+print('Sesudah diubah dalam format datetime:\n', gaq.head())
 # %%
