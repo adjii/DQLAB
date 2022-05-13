508750410704fbe1a63740e4a56b305b88e5692d
adjii <desykaadjisafitri508@gmail.com>
2022-05-13 22:34:30 +0700

    Data Manipulation

--- pandas advanced.py
+++ pandas advanced.py
@@ -339,4 +339,46 @@ print('Downsampling daily to quarterly - min (5 teratas):\n', gaq_quarterly.head
 # Upsampling dari daily to hourly dan kita hitung reratanya
 gaq_hourly = gaq.resample('H').mean()
 print('Upsampling daily to hourly - mean (5 teratas):\n', gaq_hourly.head())
+# %% [markdown]
+# __Resampling by Frequency__
+# %%
+import pandas as pd
+# Load dataset https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+print('Dataset sebelum di-resampling (5 teratas):\n', gaq.head())
+# Resample dari daily to 2 monthly, hitung reratanya, dan fillna = 'bfill'
+gaq_2monthly = gaq.resample('2M').mean().fillna(method='bfill')
+print('Resampling daily to 2 monthly - mean - ffill (5 teratas):\n', gaq_2monthly.head())
+# %% [markdown]
+# __Visualisasi__
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+# Load dataset https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+# [1] Membuat pivot table yang menunjukkan waktu di barisnya dan masing-masing value dari pollutant nya dalam kolom
+gaq_viz = gaq[['pollutant','value']].reset_index().set_index(['timestamp','pollutant'])
+gaq_viz = gaq_viz.pivot_table(index='timestamp', columns='pollutant', aggfunc='mean').fillna(0)
+gaq_viz.columns = gaq_viz.columns.droplevel(0)
+print('Data (5 teratas):\n', gaq_viz.head())
+# [2] Membuat fungsi yang memberikan default value 0 ketika value nya di bawah 0 dan apply ke setiap elemen dari dataset tersebut, kemudian menampilkannya sebagai chart
+def default_val(val):
+    if val < 0:
+        return 0
+    else:
+        return val
+line1 = gaq_viz.resample('M').mean().ffill().applymap(lambda x: default_val(x)).apply(lambda x: x/x.max()) # default value if value < 0 then 0, kemudian menghasilkan % value = value/max(value)
+line1.plot(
+    title = 'average value of each pollutant over months',
+    figsize = (10,10), #ukuran canvas 10px x 10px
+    ylim = (0,1.25), #memberikan batas tampilan y-axis hanya 0 sampai 125%
+    subplots = True #memecah plot menjadi beberapa bahan sesuai dengan jumlah kolom
+)
+plt.ylabel('avg pollutant (%)')
+plt.xlabel('month')
+plt.show()
 # %%
