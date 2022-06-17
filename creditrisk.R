9e2d0621ae604582a6c5db069d67342190bfa69c
adjii <desykaadjisafitri508@gmail.com>
2022-06-17 22:27:38 +0700

    Data Science in Finance: Credit Risk Analysis

--- creditrisk.R
+++ creditrisk.R
@@ -4,8 +4,10 @@ library("openxlsx")
 dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
 str(dataCreditRating)
 
-#Melakukan konversi kolom risk_rating menjadi factor
+#Merubah tipe data class variable sebagai factor
 dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating)
 
-#Melihat struktur setelah konversi
-str(dataCreditRating)
+#Menghilangkan beberapa variable input dari dataset
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+str(datafeed)
