d99c4dbdb47b7b579f9ed178d3e3c80bca514b18
adjii <desykaadjisafitri508@gmail.com>
2022-06-18 18:12:41 +0700

    Data Science in Finance: Credit Risk Analysis

--- creditrisk.R
+++ creditrisk.R
@@ -11,3 +11,16 @@ dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating)
 input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
 datafeed <- dataCreditRating[ , input_columns ]
 str(datafeed)
+
+#Mempersiapkan porsi index acak untuk training dan testing set
+set.seed(100)
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+str(input_training_set)
+str(class_training_set)
+str(input_testing_set)
